351bcb4150bdafe0ec86dbd0e7bc6dde66a96ebf
Justyna <kowalkowska.just@gmail.com>
2020-09-29 22:14:59 +0200

    Created a 3D Moon.

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -29,13 +29,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 //        sceneView.scene = scene
         
         
-        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+        let sphere = SCNSphere(radius: 0.2) //20 cm
         let material = SCNMaterial()
-        material.diffuse.contents = UIColor.red
-        cube.materials = [material]
+        material.diffuse.contents = UIImage(named: "art.scnassets/8k_moon.jpg")
+        sphere.materials = [material]
         let node = SCNNode()
         node.position = SCNVector3(x: 0.0, y: 0.1, z: -0.5)
-        node.geometry = cube
+        node.geometry = sphere
         sceneView.scene.rootNode.addChildNode(node)
         sceneView.autoenablesDefaultLighting = true
         
