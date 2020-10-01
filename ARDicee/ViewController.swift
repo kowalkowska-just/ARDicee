d258067c6066f43f2a41bec433ad613330e27262
Justyna <kowalkowska.just@gmail.com>
2020-10-02 00:27:15 +0200

    Animation of the all 3D dice at once when pressed in the button.

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var diceArray = [SCNNode]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -62,24 +64,42 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                         z: hitResult.worldTransform.columns.3.z
                     )
                     
+                    diceArray.append(diceNode)
+                    
                     sceneView.scene.rootNode.addChildNode(diceNode)
                     
+                    roll(dice: diceNode)
                     
-                    let randomX = Float(arc4random_uniform(4) + 1) * (Float.pi / 2)
-                    let randomZ = Float(arc4random_uniform(4) + 1) * (Float.pi / 2)
-                    
-                    diceNode.runAction(
-                        SCNAction.rotateTo(
-                            x: CGFloat(randomX * 5),
-                            y: 0,
-                            z: CGFloat(randomZ * 5),
-                            duration: 0.6)
-                        )
                 }
             }
         }
     }
     
+    func rollAll() {
+        if !diceArray.isEmpty {
+            for dice in diceArray {
+                roll(dice: dice)
+            }
+        }
+    }
+    
+    func roll(dice: SCNNode) {
+        
+        let randomX = Float(arc4random_uniform(4) + 1) * (Float.pi / 2)
+        let randomZ = Float(arc4random_uniform(4) + 1) * (Float.pi / 2)
+        
+        dice.runAction(
+            SCNAction.rotateTo(
+                x: CGFloat(randomX * 5),
+                y: 0,
+                z: CGFloat(randomZ * 5),
+                duration: 0.6)
+            )
+    }
+    
+    @IBAction func rollAgain(_ sender: UIBarButtonItem) {
+        rollAll()
+    }
     
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
