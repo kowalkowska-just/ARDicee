c0fc98bb1413a2b440b0c5aceb8cb75e34892bb1
Justyna <kowalkowska.just@gmail.com>
2020-10-01 17:53:32 +0200

    Create a dice model.

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -16,30 +16,36 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        // Set the view's delegate
-//        sceneView.delegate = self
-//
-//        // Show statistics such as fps and timing information
-//        sceneView.showsStatistics = true
-//
-//        // Create a new scene
-//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-//
-//        // Set the scene to the view
-//        sceneView.scene = scene
+        // Set the view's delegate
+        sceneView.delegate = self
         
+        //Create a new scene
         
-        let sphere = SCNSphere(radius: 0.2) //20 cm
-        let material = SCNMaterial()
-        material.diffuse.contents = UIImage(named: "art.scnassets/8k_moon.jpg")
-        sphere.materials = [material]
-        let node = SCNNode()
-        node.position = SCNVector3(x: 0.0, y: 0.1, z: -0.5)
-        node.geometry = sphere
-        sceneView.scene.rootNode.addChildNode(node)
-        sceneView.autoenablesDefaultLighting = true
+        let diceScene = SCNScene(named: "art.scnassets/diceCollada copy.scn")!
+        
+        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+        
+        diceNode.position = SCNVector3(x: 0.0, y: 0.0, z: -0.1)
+        
+        sceneView.scene.rootNode.addChildNode(diceNode)
+        }
         
     }
     
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        
+        //Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        //Pause the view's session
+        sceneView.session.pause()
+    }
 }
