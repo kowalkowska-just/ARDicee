6556eaaf23f361c6732dc5d6568dbe6032548c04
Justyna <kowalkowska.just@gmail.com>
2020-10-02 00:32:31 +0200

    Animation of all 3D dice at once when shaked a phone.

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -101,6 +101,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         rollAll()
     }
     
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        rollAll()
+    }
+    
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         
